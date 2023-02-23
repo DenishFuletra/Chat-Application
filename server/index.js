e2527fab0de148f5161fc8d83c6e341c1a35d961
Denish Fuletra <fuletradenish@gmail.com>
2023-02-23 12:51:03 +0530

    added chakra ui

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
 const app = express();
 dotenv.config();
+app.use(cors());
+app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send("api is running")
-})
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
 
 const port = process.env.PORT;
-app.listen(port,()=>{
-    console.log("Server is started");
-})
+app.listen(port, () => {
+  console.log("Server is started");
+});
