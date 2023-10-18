8bc9f0ab16d65ff33ec0c9a94acef5ab15ea79dc
Denish Fuletra <fuletradenish@gmail.com>
2023-10-18 23:23:12 +0530

    commit

--- client/src/component/modal/otpModal.jsx
+++ client/src/component/modal/otpModal.jsx
@@ -16,7 +16,7 @@ import {
     ModalFooter,
     useToast
 } from '@chakra-ui/react';
-import api from '../../App';
+import axios from 'axios';
 
 export default function OtpModal({ signup, setSignup, children, isOpen, onClose }) {
     const { onOpen } = useDisclosure()
@@ -40,7 +40,7 @@ export default function OtpModal({ signup, setSignup, children, isOpen, onClose 
         formData.append('otp', otp);
 
         try {
-            const response = await api.post(`${process.env.REACT_APP_BASEURL}/api/user/signup`, formData);
+            const response = await axios.post(`${process.env.REACT_APP_BASEURL}/api/user/signup`, formData);
             console.log(response);
             toast({
                 title: response.data.message,
